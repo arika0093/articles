5da6f36a84a7094cfd1d28d550c30d0f786a4307
Arika Ishinami <delete0093@gmail.com>
2025-09-12 22:57:03 +0900

    create new branch for zenn

--- setup.sh
+++
@@ -1,9 +0,0 @@
-alias cc="claude --dangerously-skip-permissions"
-# sudo npx playwright install-deps
-claude mcp add playwright npx @playwright/mcp@latest
-# https://github.com/web-agent-master/google-search
-# cd /mcp/google-search
-# npm install
-# npm run build
-# npm link
-# claude mcp add google-search npx google-search-mcp
