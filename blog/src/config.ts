babfc34645009c1f73cf9cbeeb5a9041d38dab7a
Arika Ishinami <delete0093@gmail.com>
2026-01-13 02:26:08 +0000

    Update site description and social links; add OG image generation endpoint

--- blog/src/config.ts
+++ blog/src/config.ts
@@ -2,7 +2,7 @@ export const SITE = {
   website: "https://arika0093.github.io/articles/",
   author: "arika0093",
   profile: "https://github.com/arika0093",
-  desc: "技術記事とブログ",
+  desc: "A blog about programming and technology.",
   title: "arika0093's blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
